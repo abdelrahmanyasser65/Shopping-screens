ec67c3b0844043cc8cb301d3e4e3d7c2f9bf9a29
abdelrahmanyassero <abdelrahman117ma@gmail.com>
2022-06-13 13:49:07 +0200

    13/6=>2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping1/screens/p4.dart';
 import 'package:shopping1/screens/p6.dart';
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:Shopping6(),
+      home:Shopping4(),
     );
   }
 }
